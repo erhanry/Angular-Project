3a8dfc7f8163edd060fc1a094ad00b1222bf9d26
Erhan Ysuf <erhan.ysuf@gmail.com>
2024-03-14 01:50:13 +0200

    Edit App Module

--- bookstore/src/app/app.module.ts
+++ bookstore/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, CoreModule, UserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
