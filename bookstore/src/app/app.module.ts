2f01183bb97fc00da97e2664b5dc15d74c02187e
Erhan Ysuf <erhan.ysuf@gmail.com>
2024-03-18 00:59:18 +0200

    Set Lazy Load User Module

--- bookstore/src/app/app.module.ts
+++ bookstore/src/app/app.module.ts
@@ -4,20 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { UserModule } from './user/user.module';
 import { AppInterceptorProvider } from './app.interceptor';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [
-    BrowserModule,
-    CoreModule,
-    UserModule,
-    AppRoutingModule,
-    HttpClientModule,
-  ],
+  imports: [BrowserModule, HttpClientModule, CoreModule, AppRoutingModule],
   providers: [AppInterceptorProvider],
   bootstrap: [AppComponent],
 })
--- bookstore/src/app/user/user.module.ts
+++ bookstore/src/app/user/user.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { SharedModule } from '../shared/shared.module';
+import { UserRoutingModule } from './user-routing.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, UserRoutingModule],
   exports: [LoginComponent, RegisterComponent],
 })
 export class UserModule {}
