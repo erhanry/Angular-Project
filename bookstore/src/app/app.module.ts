fe4c6788784c8615fdafd0c5e4a8ca5c0d17d85f
Erhan Ysuf <erhan.ysuf@gmail.com>
2024-03-18 00:26:13 +0200

    Add AppInterceptor
    ng generate interceptor app

--- bookstore/src/app/app.module.ts
+++ bookstore/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { UserModule } from './user/user.module';
+import { AppInterceptorProvider } from './app.interceptor';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, UserModule, AppRoutingModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    UserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [AppInterceptorProvider],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
