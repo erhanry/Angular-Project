5e1e603c5bfd718f37fc3f46d008b0f513d6c0a4
Erhan Ysuf <erhan.ysuf@gmail.com>
2024-03-18 01:00:22 +0200

    Configure App Routing Module

--- bookstore/src/app/app-routing.module.ts
+++ bookstore/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./user/user.module').then((module) => module.UserModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
