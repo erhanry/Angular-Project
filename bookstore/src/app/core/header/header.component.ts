de379834e75a616fb6a18a9dac47815e47df40bd
Erhan Ysuf <erhan.ysuf@gmail.com>
2024-03-23 20:45:44 +0200

    Add Toggle Navbar

--- bookstore/src/app/core/header/header.component.ts
+++ bookstore/src/app/core/header/header.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isToggleNavbar: boolean = false;
+
+  showNavbar() {
+    this.isToggleNavbar = !this.isToggleNavbar;
+  }
+}
